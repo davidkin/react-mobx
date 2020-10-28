2f3eb7de6cf47da962d0b1753246092bbac28c1c
David Potapkin <david.potapkin@dataart.com>
2020-10-28 13:43:08 +0200

    develop, add serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { configure } from "mobx";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { configure } from 'mobx';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 configure({
   computedRequiresReaction: true,
   observableRequiresReaction: true,
   reactionRequiresObservable: true,
-  enforceActions: "always",
+  enforceActions: 'always',
 });
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
+
+serviceWorker.register();
